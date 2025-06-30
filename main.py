d6ab741eea633c7f76b674f916188cbc5d0bf133
Pranav Garg <pranavgarg9697@gmail.com>
2025-07-01 00:22:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import yaml
-from tools import tool_weather
+from tools import tool_weather,ml_tools
 from langchain.agents import Tool, initialize_agent, AgentType
 
 load_dotenv()
@@ -16,21 +16,23 @@ def get_config(path='config.yaml'):
 
 config = get_config()
 
-# Define the tool
-weather_tool = Tool(
-    name="WeatherFetcher",
-    func=tool_weather.get_weather,
-    description="Use this to get weather info by city name."
-)
+tools = [
+    Tool(name="Read Data", func=ml_tools.run_read_data, description="Read Dataset from a CSV file."),
+    Tool(name="EDA", func=ml_tools.run_eda, description="Performs exploratory data analysis."),
+    Tool(name="FeatureSelection", func=ml_tools.run_feature_selection, description="Selects important features."),
+    Tool(name="Heatmap", func=ml_tools.run_heatmap, description="Creates a correlation heatmap."),
+    Tool(name="WeatherFetcher",func=tool_weather.get_weather,description="Use this to get weather info by city name."),
+]
 
 # Create the agent
 llm = ChatGoogleGenerativeAI(model=config['google_model_name'],google_api_key='')
 agent = initialize_agent(
-    tools=[weather_tool],
+    tools=tools,
     llm=llm,
     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     verbose=True,
 )
 
 # Use it
-agent.invoke("What is the weather in Delhi?")
+query = input("Ask something related to ML analysis: ")
+print(agent.run(query))
